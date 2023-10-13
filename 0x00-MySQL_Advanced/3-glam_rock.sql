8f7e5d44ae9cb6528031b284d636204d5ebe4614
Merdasa Belay <merdasabelay@gmail.com>
2023-10-13 03:37:40 +0300

    Features added

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,13 @@
 -- Write a SQL script that lists all bands 
 --with Glam rock as their main style, ranked by their longevity
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
-FROM metal_bands 
-WHERE style LIKE '%Glam rock%';
+SELECT band_name,
+       CASE
+           WHEN split IS NOT NULL AND formed IS NOT NULL THEN split - formed
+           WHEN split IS NOT NULL THEN split - MIN_YEAR(formed)
+           WHEN formed IS NOT NULL THEN MAX_YEAR(split) - formed
+           ELSE NULL
+       END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan;
+
