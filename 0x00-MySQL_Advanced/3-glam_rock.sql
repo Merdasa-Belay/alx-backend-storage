f9657778cf2df96db418838e1c6a553e437f6fc7
Merdasa Belay <merdasabelay@gmail.com>
2023-10-13 03:58:06 +0300

    Features added

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,11 +3,10 @@
 SELECT band_name,
        CASE
            WHEN split IS NOT NULL AND formed IS NOT NULL THEN split - formed
-           WHEN split IS NOT NULL THEN split - MIN_YEAR(formed)
-           WHEN formed IS NOT NULL THEN MAX_YEAR(split) - formed
-           ELSE NULL
+           WHEN split IS NOT NULL THEN 2022 - YEAR(formed)
+           WHEN formed IS NOT NULL THEN YEAR(split) - YEAR(formed)
+           ELSE 0
        END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan;
-
+ORDER BY lifespan DESC;
